528cc4b7393aaca22abd5efd77df7173713ba0b9
Oleh <666zakar.15@gmail.com>
2023-10-07 21:02:42 +0300

    task-2 done

--- task-2/script.js
+++ task-2/script.js
@@ -1,22 +1,36 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const img = document.querySelectorAll("img");
+// Current image and date
+const img = document.querySelectorAll(".image-item");
 
-  const imgLenght = img.length;
+const imgLenght = img.length;
 
-  let currentDate = new Date();
-  let formattedDate = currentDate.toLocaleDateString("uk-UA", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-
-  const infoLenght = document.getElementById("currentImg");
-  infoLenght.textContent = "Кількість зображень:" + " " + imgLenght;
-
-  const infoDate = document.getElementById("currentDate");
-  infoDate.textContent = "Дата та час:" + " " + formattedDate;
+let currentDate = new Date();
+let formattedDate = currentDate.toLocaleDateString("uk-UA", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
 });
+
+const infoLenght = document.getElementById("currentImg");
+infoLenght.textContent = "Кількість зображень:" + " " + imgLenght;
+
+const infoDate = document.getElementById("currentDate");
+infoDate.textContent = "Дата та час: " + "  " + formattedDate;
+
+// open/close modal
+function openImg(img) {
+  const modal = document.getElementById("modal");
+  const modalImage = document.getElementById("modalImage");
+
+  modal.style.display = "block";
+  modalImage.style.borderRadius = "10px";
+  modalImage.src = img.src;
+}
+
+function closeImg() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
